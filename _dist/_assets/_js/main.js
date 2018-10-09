6624d8079d57c0cf634539813bc81a7f64aa1222
Thomas Claude <thomasclaude@mbp-de-thomas.epic-sys.io>
2018-10-09 16:59:44 +0200

    :heavy_plus_sign: Adding dependencies :construction:

--- _dist/_assets/_js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.info('✌️ This website was made by www.thomasclaude.be . Before copying feel free to ask if you dont understand something.✌️');
-});
-
-
-
-
-
--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,117 @@
-var gulp = require('gulp');
-var connect = require ("gulp-connect");
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const connect = require ("gulp-connect");
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const pug = require('gulp-pug');
+const chalk = require('chalk');
+const imagemin = require('gulp-imagemin');
 
 
-//dossier dev
-//validation html/css
-//sass
-//fichiers de travail
-//dossier prod
-//prefixer css
-//concat js, css
-//minifier js, css
-//compresser auto vos images
+/*
+ * With this gulp file you are able to
+ * Compiling Sass/ES6/Pug/Images
+ * Putting on your files after compile in dist folder
+ * Having a webserver with livereload on custom port
+ *
+ * This is a informative sentence, you're not supposed to remove it.
+ * Hope you will respect my wokr.
+ */
 
 gulp.task('hello', function(){
-	console.log("hello world")
-});
-
-//rafraichir
-gulp.task('watch', function(){
-	gulp.watch("*.html", ["html"])
-	gulp.watch("./_assets/_scss/*.scss", ["compileSass"])
-	gulp.watch("./_assets/_js/*.js", ["js"]);
-	
+	console.log(chalk.bgHex('#2e67d7').hex('#ffffff')('✌️ Hello, this workflow was made by'), chalk.bold.bgHex('#51a982').hex('#ffffff')(' www.thomasclaude.be ') );
+	console.log(chalk.bgHex('#2e67d7').hex('#ffffff')('I am glad to see that you are using it and hope you enjoy it.'));
+	console.log(chalk.bold.bgHex('#f30f69').hex('#ffffff')('If you have any reports/bug you can send me a mail at coffee@thomasclaude.be ✌️ '));
 });
 
 
-//Live reload
+// Live reload, watching all files and if change call another gulp task.
 
-gulp.task("compileSass", function(){
-	return gulp.src("./_assets/_scss/*.*ss")
-	.pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./_dist/_assets/_css'))
-	.pipe(gulp.dest('./_assets/_css'))
-	.pipe(connect.reload());
-	
+gulp.task('watch', function() {
+	gulp.watch("./src/views/*.pug", ["views"])
+	gulp.watch("./src/*.html", ["html"])
+	gulp.watch("./src/assets/styles/scss/*", ["compileSass"])
+	gulp.watch("./src/assets/scripts/js/*.js", ["compileJs"])
+	gulp.watch("./src/assets/images/*", ["compileAssets"])
+
 });
-gulp.task("js", function(){
-	return gulp.src("./_assets/_js/*.js")
-	.pipe(gulp.dest('./_dist/_assets/_js'))
-	.pipe(gulp.dest('./_assets/_js'))
-	.pipe(connect.reload());
-	
+
+
+// Other tasks called by live reload.
+
+/*
+ * After checking if changes has been done in the scss
+ * file then compiling and putting it on the dist as css file.
+ */
+
+gulp.task("compileSass", () =>
+	gulp.src("./src/assets/styles/scss/*.*ss")
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./dist/assets/styles/'))
+		// .pipe(gulp.dest('./src/assets/styles/css/'))
+		.pipe(connect.reload())
+
+);
+
+/*
+ * After checking if changes has been done in the image
+ * file then compiling and putting it on the dist.
+ */
+
+gulp.task('compileAssets', () =>
+	gulp.src('./src/assets/images/*')
+		.pipe(imagemin([
+			imagemin.svgo({ plugins: [{ removeViewBox: true }] })
+		], {
+				verbose: true
+			}))
+			.pipe(gulp.dest('./dist/assets/images'))
+			.pipe(connect.reload())
+);
+
+
+/*
+ * After checking if changes has been done in the script file
+ * then compiling ES6 to javascript it on the dist.
+ */
+
+gulp.task('compileJs', () =>
+	gulp.src('./src/assets/scripts/js/*.js')
+			.pipe(babel())
+			.pipe(gulp.dest('./dist/assets/scripts/js'))
+			.pipe(connect.reload())
+);
+
+/*
+ * After checking if changes has been done in the views file
+ * then compiling pug to html it on the dist.
+ */
+
+gulp.task('views', function buildHTML() {
+	return gulp.src('./src/views/*.pug')
+		.pipe(pug({
+			// Your options in here.
+		}))
+		.pipe(gulp.dest('./dist/'))
+		// .pipe(gulp.dest('./src'))
+		.pipe(connect.reload())
 });
-gulp.task("html", function(){
-	return gulp.src("*.html")
-	.pipe(gulp.dest('./_dist/'))
-	.pipe(gulp.dest('./'))
-	.pipe(connect.reload());
-	
-});
+
+gulp.task("html", () =>
+	gulp.src("./src/*.html")
+			.pipe(gulp.dest('./dist/'))
+			// .pipe(gulp.dest('./src'))
+			.pipe(connect.reload())
+
+);
+
+// Creating a webserver from the dist file on the 8080 port
 
 gulp.task("connect", function(){
 	connect.server({
-		root: [__dirname],
+		root: 'dist',
 		livereload: true,
 		port: 8080
 	});
 });
 
-gulp.task("default", ["connect", "watch"]);
+gulp.task("default", ["hello", "connect", "watch"]);
