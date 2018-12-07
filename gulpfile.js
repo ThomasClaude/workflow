32a6f2fc4930b6fabe2f7551a722eaa644e2cb73
Thomas Claude <thomasclaude@mbp-de-thomas.epic-sys.io>
2018-12-07 15:32:09 +0100

    :building_construction: change

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const connect = require ("gulp-connect");
+const connect = require('gulp-connect');
 const sass = require('gulp-sass');
 const babel = require('gulp-babel');
 const pug = require('gulp-pug');
@@ -17,24 +17,25 @@ const imagemin = require('gulp-imagemin');
  * Hope you will respect my wokr.
  */
 
-gulp.task('hello', function(){
-	console.log(chalk.bgHex('#2e67d7').hex('#ffffff')('✌️ Hello, this workflow was made by'), chalk.bold.bgHex('#51a982').hex('#ffffff')(' www.thomasclaude.be ') );
-	console.log(chalk.bgHex('#2e67d7').hex('#ffffff')('I am glad to see that you are using it and hope you enjoy it.'));
-	console.log(chalk.bold.bgHex('#f30f69').hex('#ffffff')('If you have any reports/bug you can send me a mail at coffee@thomasclaude.be ✌️ '));
+gulp.task('hello', () => {
+  console.log(chalk.bgHex('#2e67d7').hex('#ffffff')('✌️ Hello, this workflow was made by'), chalk.bold.bgHex('#51a982').hex('#ffffff')(' www.thomasclaude.be ') );
+  console.log(chalk.bgHex('#2e67d7').hex('#ffffff')('I am glad to see that you are using it and hope you enjoy it.'));
+  console.log(chalk.bold.bgHex('#f30f69').hex('#ffffff')('If you have any reports/bug you can send me a mail at coffee@thomasclaude.be ✌️ '));
 });
 
 
 // Live reload, watching all files and if change call another gulp task.
 
-gulp.task('watch', function() {
-	gulp.watch("./src/views/*.pug", ["views"])
-	gulp.watch("./src/assets/components/*.pug", ["views"])
-	gulp.watch("./src/*.html", ["html"])
-	gulp.watch("./src/assets/styles/scss/*", ["compileSass"])
-	gulp.watch("./src/assets/styles/scss/*/*", ["compileSass"])
-	gulp.watch("./src/assets/scripts/js/*.js", ["compileJs"])
-	gulp.watch("./src/assets/images/*", ["compileAssets"])
-
+gulp.task('watch', () => {
+  gulp.watch('./src/views/*.pug', ['views']);
+  gulp.watch('./src/assets/components/*/*.pug', ['views']);
+  gulp.watch('./src/assets/components/*', ['views']);
+  gulp.watch('./src/*.html', ['html']);
+  gulp.watch('./src/assets/styles/scss/*', ['compileSass']);
+  gulp.watch('./src/assets/styles/scss/*/*', ['compileSass']);
+  gulp.watch('./src/assets/scripts/js/*.js', ['compileJs']);
+  gulp.watch('./src/assets/scripts/js/*/*.js', ['compileJs']);
+  gulp.watch('./src/assets/images/*', ['compileAssets']);
 });
 
 
@@ -45,12 +46,11 @@ gulp.task('watch', function() {
  * file then compiling and putting it on the dist as css file.
  */
 
-gulp.task("compileSass", () =>
-	gulp.src("./src/assets/styles/scss/*.*ss")
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./dist/assets/styles/'))
-		.pipe(connect.reload())
-
+gulp.task('compileSass', () =>
+  gulp.src('./src/assets/styles/scss/*.*ss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/assets/styles/'))
+    .pipe(connect.reload())
 );
 
 /*
@@ -59,14 +59,14 @@ gulp.task("compileSass", () =>
  */
 
 gulp.task('compileAssets', () =>
-	gulp.src('./src/assets/images/*')
-		.pipe(imagemin([
-			imagemin.svgo({ plugins: [{ removeViewBox: true }] })
-		], {
-				verbose: true
-			}))
-			.pipe(gulp.dest('./dist/assets/images'))
-			.pipe(connect.reload())
+  gulp.src('./src/assets/images/*')
+    .pipe(imagemin([
+      imagemin.svgo({ plugins: [{ removeViewBox: true }] }),
+    ], {
+      verbose: true,
+    }))
+    .pipe(gulp.dest('./dist/assets/images'))
+    .pipe(connect.reload())
 );
 
 
@@ -76,10 +76,10 @@ gulp.task('compileAssets', () =>
  */
 
 gulp.task('compileJs', () =>
-	gulp.src('./src/assets/scripts/js/*.js')
-			.pipe(babel())
-			.pipe(gulp.dest('./dist/assets/scripts/js'))
-			.pipe(connect.reload())
+  gulp.src('./src/assets/scripts/js/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./dist/assets/scripts/js'))
+    .pipe(connect.reload())
 );
 
 /*
@@ -87,30 +87,30 @@ gulp.task('compileJs', () =>
  * then compiling pug to html it on the dist.
  */
 
-gulp.task('views', function buildHTML() {
-	return gulp.src('./src/views/*.pug')
-		.pipe(pug({
-			// Your options in here.
-		}))
-		.pipe(gulp.dest('./dist/'))
-		.pipe(connect.reload())
+gulp.task('views', () => {
+  return gulp.src('./src/views/*.pug')
+    .pipe(pug({
+      // Your options in here.
+    }))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(connect.reload())
 });
 
-gulp.task("html", () =>
-	gulp.src("./src/*.html")
-			.pipe(gulp.dest('./dist/'))
-			.pipe(connect.reload())
+gulp.task('html', () =>
+  gulp.src('./src/*.html')
+    .pipe(gulp.dest('./dist/'))
+    .pipe(connect.reload())
 
 );
 
 // Creating a webserver from the dist file on the 8080 port
 
-gulp.task("connect", function(){
-	connect.server({
-		root: 'dist',
-		livereload: true,
-		port: 8080
-	});
+gulp.task('connect', () => {
+  connect.server({
+    root: 'dist',
+    livereload: true,
+    port: 8080
+  });
 });
 
-gulp.task("default", ["hello", "connect", "watch"]);
+gulp.task('default', ['hello', 'connect', 'watch']);
