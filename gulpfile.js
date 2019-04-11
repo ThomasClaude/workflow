6890a24e19790931a8ea6ea1f579e68659b2169d
Thomas Claude <coffee@thomasclaude.be>
2019-04-11 11:37:46 +0200

    :art: Updating the gulpfile to manage new and deleted files

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,8 @@ sass.compiler = require('node-sass');
 
 // Variables related to dir
 
-const srcDir = './src/';
-const distDir = './dist/';
+const srcDir = 'src/';
+const distDir = 'dist/';
 
 /*
  * With this gulp file you are able to
@@ -28,25 +28,25 @@ const distDir = './dist/';
  */
 
 gulp.task('hello', () => {
-	console.log(chalk.bgHex('#2e67d7').hex('#ffffff')('✌️ Hello, this workflow was made by'), chalk.bold.bgHex('#51a982').hex('#ffffff')(' www.thomasclaude.be '));
-	console.log(chalk.bgHex('#2e67d7').hex('#ffffff')('I am glad to see that you are using it and hope you enjoy it.'));
-	console.log(chalk.bold.bgHex('#f30f69').hex('#ffffff')('If you have any reports/bug you can send me a mail at coffee@thomasclaude.be ✌️ '));
+  console.log(chalk.bgHex('#2e67d7').hex('#ffffff')('✌️ Hello, this workflow was made by'), chalk.bold.bgHex('#51a982').hex('#ffffff')(' www.thomasclaude.dev '));
+  console.log(chalk.bgHex('#2e67d7').hex('#ffffff')('I am glad to see that you are using it and hope you enjoy it.'));
+  console.log(chalk.bold.bgHex('#f30f69').hex('#ffffff')('If you have any reports/bug you can send me a mail at coffee@thomasclaude.be ✌️ '));
 });
 
 
 // Live reload, watching all files and if change call another gulp task.
 
 gulp.task('watch', () => {
-	gulp.watch(`${srcDir}views/*.pug`, ['views']);
-	gulp.watch(`${srcDir}assets/components/*/*.pug`, ['views']);
-	gulp.watch(`${srcDir}assets/components/*`, ['views']);
-	gulp.watch(`${srcDir}*.html`, ['html']);
-	gulp.watch(`${srcDir}assets/styles/scss/*`, ['css']);
-	gulp.watch(`${srcDir}assets/styles/scss/*/*`, ['css']);
-	gulp.watch(`${srcDir}assets/styles/scss/components/*/*`, ['css']);
-	gulp.watch(`${srcDir}assets/scripts/*.js`, ['compileJs']);
-	gulp.watch(`${srcDir}assets/scripts/*/*.js`, ['compileJs']);
-	gulp.watch(`${srcDir}assets/images/*`, ['compileAssets']);
+  gulp.watch(`${srcDir}views/*.pug`, ['views']);
+  gulp.watch(`${srcDir}assets/components/*/*.pug`, ['views']);
+  gulp.watch(`${srcDir}assets/components/*`, ['views']);
+  gulp.watch(`${srcDir}*.html`, ['html']);
+  gulp.watch(`${srcDir}assets/styles/scss/*`, ['css']);
+  gulp.watch(`${srcDir}assets/styles/scss/*/*`, ['css']);
+  gulp.watch(`${srcDir}assets/styles/scss/components/*/*`, ['css']);
+  gulp.watch(`${srcDir}assets/scripts/*.js`, ['compileJs']);
+  gulp.watch(`${srcDir}assets/scripts/*/*.js`, ['compileJs']);
+  gulp.watch(`${srcDir}assets/images/*`, ['compileAssets']);
 });
 
 
@@ -58,23 +58,23 @@ gulp.task('watch', () => {
  */
 
 gulp.task('compileSass', () =>
-	gulp.src(`${srcDir}assets/styles/scss/*.*ss`)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(`${distDir}assets/styles/`))
-		.pipe(connect.reload())
+  gulp.src(`${srcDir}assets/styles/scss/*.*ss`)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(`${distDir}assets/styles/`))
+    .pipe(connect.reload())
 );
 
 gulp.task('css', () => {
-	gulp
-		.src(`${srcDir}assets/styles/scss/main.scss`)
-		.pipe(bulkSass())
-		.pipe(
-			sass({
-				includePaths: ['src/styles/scss/*'],
-			}))
-		.on('error', sass.logError)
-		.pipe(gulp.dest(`${distDir}assets/styles/`))
-		.pipe(connect.reload());
+  gulp
+    .src(`${srcDir}assets/styles/scss/main.scss`)
+    .pipe(bulkSass())
+    .pipe(
+      sass({
+        includePaths: ['src/styles/scss/*'],
+      }))
+    .on('error', sass.logError)
+    .pipe(gulp.dest(`${distDir}assets/styles/`))
+    .pipe(connect.reload());
 });
 
 /*
@@ -83,14 +83,14 @@ gulp.task('css', () => {
  */
 
 gulp.task('compileAssets', () =>
-	gulp.src(`${srcDir}assets/images/*`)
-		.pipe(imagemin([
-			imagemin.svgo({ plugins: [{ removeViewBox: true }] }),
-		], {
-			verbose: true,
-		}))
-		.pipe(gulp.dest(`${distDir}assets/images`))
-		.pipe(connect.reload())
+  gulp.src(`${srcDir}assets/images/*`)
+    .pipe(imagemin([
+      imagemin.svgo({ plugins: [{ removeViewBox: true }] }),
+    ], {
+      verbose: true,
+    }))
+    .pipe(gulp.dest(`${distDir}assets/images`))
+    .pipe(connect.reload())
 );
 
 
@@ -100,10 +100,10 @@ gulp.task('compileAssets', () =>
  */
 
 gulp.task('compileJs', () =>
-	gulp.src(`${srcDir}assets/scripts/*.js`)
-		.pipe(babel())
-		.pipe(gulp.dest(`${distDir}assets/scripts/`))
-		.pipe(connect.reload())
+  gulp.src(`${srcDir}assets/scripts/*.js`)
+    .pipe(babel())
+    .pipe(gulp.dest(`${distDir}assets/scripts/`))
+    .pipe(connect.reload())
 );
 
 /*
@@ -112,28 +112,28 @@ gulp.task('compileJs', () =>
  */
 
 gulp.task('views', () => {
-	gulp.src(`${srcDir}views/*.pug`)
-		.pipe(pug())
-		.on('error', gutil.log)
-		.pipe(gulp.dest(distDir))
-		.pipe(connect.reload());
+  gulp.src(`${srcDir}views/*.pug`)
+    .pipe(pug())
+    .on('error', gutil.log)
+    .pipe(gulp.dest(distDir))
+    .pipe(connect.reload());
 });
 
 gulp.task('html', () =>
-	gulp.src(`${srcDir}*.html`)
-		.pipe(gulp.dest(distDir))
-		.pipe(connect.reload())
+  gulp.src(`${srcDir}*.html`)
+    .pipe(gulp.dest(distDir))
+    .pipe(connect.reload())
 
 );
 
 // Creating a webserver from the dist file on the 8080 port
 
 gulp.task('connect', () => {
-	connect.server({
-		root: 'dist',
-		livereload: true,
-		port: 8080,
-	});
+  connect.server({
+    root: 'dist',
+    livereload: true,
+    port: 8080,
+  });
 });
 
 gulp.task('default', ['hello', 'connect', 'watch']);
