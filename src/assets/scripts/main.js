5528250d27c9e1b073b4297c82850580026af40f
Thomas Claude <coffee@thomasclaude.be>
2019-04-11 11:38:53 +0200

    :art: Setting tab into spaces

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,9 +2,9 @@
 const Barba = require('barba.js');
 
 document.addEventListener('DOMContentLoaded', () => {
-	const authorSite = 'http://thomasclaude.be';
+  const authorSite = 'http://thomasclaude.be';
 
-	console.info(`✌️ This website was made by ${authorSite} ✌️ `);
+  console.info(`✌️ This website was made by ${authorSite} ✌️ `);
 
-	Barba.Pjax.start();
+  Barba.Pjax.start();
 });
